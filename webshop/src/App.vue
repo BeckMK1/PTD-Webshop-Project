3a3ede892eef013e513f878d257638fb5cad4a2b
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-05-16 13:11:33 +0200

    header done and started on home page

--- webshop/src/App.vue
+++ webshop/src/App.vue
@@ -7,120 +7,191 @@
       <div id="round-icons">
         <div class="shop-circle">
           <div class="shop-img">
-          <img src="./assets/shopping-cart.png" alt="">
+            <img src="./assets/shopping-cart.png" alt="">
           </div>
+        </div>
+        <div class="help-circle">
+          <div class="help-img">
+            <img src="./assets/help.png" alt="">
           </div>
+        </div>
+        <div class="profile-circle">
+          <div class="profile-img">
+            <img src="./assets/profile.png" alt="">
+          </div>
+        </div>
       </div>
     </div>
     <div id="nav">
       <div class="all">
-      <img class="lines" src="./assets/3-lines-sq.svg" alt="lines">
-      <p>All products</p>
+        <img class="lines" src="./assets/3-lines-sq.svg" alt="lines">
+        <p>All products</p>
       </div>
 
       <router-link to="/clothing">Clothing</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
-*{
-margin: 0;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#header{
-display: flex;
-justify-content: start;
-background: black;
-margin-top: -15px;
-}
-#header img{
-position: relative;
-z-index: 3;
-left: 15px;
-top: 25px;
-}
-#search{
-width: 25%;
-height: 100%;
-padding: 10px;
-}
-.circle{
-position: absolute;
-background-color: black;
-padding: 49px;
-border-radius: 180px;
-z-index: 2;
-left:10px;
-top: 5px;
-}
-#nav {
-  position: relative;
-  background-color: gray;
-  z-index:1;
-  display: flex;
-  justify-content: start;
-  height: 50px;
-  width: 100%;
-  align-items: center;
-  font-size: 20px;
-  color: black;
-}
-#nav p{
+  * {
+    margin: 0;
+  }
 
-margin-right: 12px;
-}
-.all{
-border-right: black solid thin;
-height: 100%;
-display: flex;
-align-items: center;
-margin-right: 20px;
-margin-left: 110px;
-}
-.lines{
-width: 20px;
-height: 20px;
-margin-right: 5px;
-opacity: 80%;
-}
-#nav a {
-  color: black;
-  text-decoration: none;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav a.router-link-exact-active {
-  color: black;
-}
-#round-icons{
-width: 100%;
-height:auto;
+  #header {
+    display: flex;
+    justify-content: start;
+    background: black;
+    margin-top: -15px;
+  }
 
-}
-.shop-circle{
-float: right;
-background: white;
-border: solid white; 
-width: 60px;
-height: 60px;
-margin-right: 12px;
-margin-top: 20px;
-border-radius: 180px;
-}
-.shop-img{
-position: relative;
-right: 15px;
-bottom: 15px;
-}
-.shop-img img{
-height: 40px;
-width: 40px;
-}
+  #header img {
+    position: relative;
+    z-index: 3;
+    left: 15px;
+    top: 25px;
+  }
+
+  #search {
+    position: relative;
+    width: 50%;
+    height: 100%;
+    padding: 10px;
+    top: 35px;
+    margin-left: 250px;
+  }
+
+  .circle {
+    position: absolute;
+    background-color: black;
+    padding: 49px;
+    border-radius: 180px;
+    z-index: 2;
+    left: 10px;
+    top: 5px;
+  }
+
+  #nav {
+    position: relative;
+    background-color: gray;
+    z-index: 1;
+    display: flex;
+    justify-content: start;
+    height: 50px;
+    width: 100%;
+    align-items: center;
+    font-size: 20px;
+    color: black;
+  }
+
+  #nav p {
+
+    margin-right: 12px;
+  }
+
+  .all {
+    border-right: black solid thin;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    margin-right: 20px;
+    margin-left: 110px;
+  }
+
+  .lines {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+    opacity: 80%;
+  }
+
+  #nav a {
+    color: black;
+    text-decoration: none;
+  }
+
+  #nav a.router-link-exact-active {
+    color: black;
+  }
+
+  #round-icons {
+    width: 100%;
+    height: auto;
+    margin-top: 20px;
+  }
+
+  .shop-circle {
+    position: relative;
+    float: right;
+    background: white;
+    border: solid white;
+    width: 60px;
+    height: 60px;
+    border-radius: 180px;
+    margin-right: 12px;
+  }
+
+  .shop-img {
+    position: relative;
+    right: 15px;
+    bottom: 15px;
+  }
+
+  .shop-img img {
+    height: 40px;
+    width: 40px;
+  }
+
+  .help-img img {
+    height: 40px;
+    width: 40px;
+  }
+
+  .help-img {
+    position: relative;
+    right: 15px;
+    bottom: 15px;
+  }
+
+  .help-circle {
+    float: right;
+    position: relative;
+    background: white;
+    border: solid white;
+    width: 60px;
+    height: 60px;
+    border-radius: 180px;
+    margin-left: 12px;
+    margin-right: 12px;
+  }
+
+  .profile-img img {
+    height: 40px;
+    width: 40px;
+  }
+
+  .profile-img {
+    position: relative;
+    right: 15px;
+    bottom: 15px;
+  }
+
+  .profile-circle {
+    float: right;
+    position: relative;
+    background: white;
+    border: solid white;
+    width: 60px;
+    height: 60px;
+    border-radius: 180px;
+  }
 </style>
--- webshop/src/views/Home.vue
+++ webshop/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <slider />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import slider from '@/components/slider.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    slider
   }
 }
 </script>
