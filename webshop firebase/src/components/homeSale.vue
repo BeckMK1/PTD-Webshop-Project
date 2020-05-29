c8a0ef06d5b66035527cd7ac3cc035a726f7b423
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-05-29 12:57:30 +0200

    on sale done

---
+++ webshop firebase/src/components/homeSale.vue
@@ -0,0 +1,141 @@
+<template>
+    <div class="sale">
+      <div class="cTitle">
+            <p>on sale</p>
+          </div>
+                <div class="cContent">
+        <article  v-for="clothing in clothings" :key="clothing.id">
+            <div class="cEach" v-if="clothing.onsale" @click="goTodetail(clothing.id)">
+          <div class="image">
+        <img :src="clothing.image">
+        </div>
+        <h3>{{clothing.name}}</h3>
+        <div class="dis">
+        <p>{{clothing.description}}</p>
+        </div>
+        <div class="stock">
+          <div class="out" v-if="clothing.instock === 0">
+            <div class="out-box"></div>
+            <p>out of stuck</p>
+          </div>
+          <div class="in" v-else="">
+            <div class="in-box"></div>
+             <p>in stock: {{clothing.instock}}</p>
+          </div>
+         
+        </div>
+        <div>
+         <p>on sale: {{clothing.off}}</p>
+        </div>
+        <div class="price-buy">
+          <p class="price">$ {{clothing.price*clothing.offcal}}</p>
+          <p class="onsale">${{clothing.price}}</p>
+          <div class="buy-btn">
+          <button>buy</button>
+          </div>
+          </div>
+        </div>
+        </article>
+       </div>
+            </div>
+</template>
+<script>
+import { ClothingRef } from "../firebase-db"
+export default {
+  name:'sale',
+  data(){
+      return{
+          clothings:[],
+   }
+},
+firestore:{
+      clothings: ClothingRef,
+  },
+  methods:{
+        goTodetail(clothingId){
+    this.$router.push({name:'details',params:{Pid:clothingId}})
+  },
+  }
+}
+</script>
+<style>
+.cContent{
+display: flex;
+flex-direction: row;
+margin-top: 50px;
+margin-left: 300px;
+user-select: none;
+cursor: pointer;
+text-align: start;
+}
+.cEach{
+display: flex;
+flex-direction: column;
+}
+.price{
+  font-weight: bold;
+  font-size: 25px;
+}
+.dis p{
+  font-size:12px;
+}
+.image img{
+  width: 25%;
+}
+h3{
+font-size: 20px;
+}
+.buy-btn button{
+  width: 40%;
+  -webkit-appearance: none;
+  border:none;
+  background: black;
+  color: white;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  border-radius: 5px;
+}
+.buy-btn{
+cursor:pointer;
+text-align:end;
+width: 20%;
+}
+.price-buy{
+display: flex;
+}
+.cTitle{
+  border-bottom: solid;
+  text-align: start;
+  width: 70%;
+  margin-left: 250px;
+  margin-top: 50px;
+  font-size: 25px;
+}
+.stock{
+  font-size: 14px;
+  display: flex;
+}
+.out{
+display: flex;
+}
+.in{
+display: flex;
+}
+.out-box{
+position: relative;
+top:2px;
+width: 12px;
+height: 12px;
+background: red;
+margin-right: 5px;
+}
+.in-box{
+position: relative;
+top:2px;
+width: 12px;
+height: 12px;
+background: green;
+margin-right: 5px; 
+}
+</style>
+
--- webshop firebase/src/views/Home.vue
+++ webshop firebase/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <slider></slider>
+    <homeSale></homeSale>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import slider from '../components/slider.vue'
+import homeSale from '../components/homeSale.vue'
 export default {
   name: 'Home',
   components: {
-    slider
+    slider,
+    homeSale
   },
 }
 </script>
